ClusterHQ/docker-plugins

extracted plugin and volume extension point specific code from original plugin_mode branch

daemon/container.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
 	"os"
 	"path"
 	"path/filepath"
@@ -30,6 +31,7 @@ import (
 	"github.com/docker/docker/pkg/networkfs/resolvconf"
 	"github.com/docker/docker/pkg/promise"
 	"github.com/docker/docker/pkg/symlink"
+	"github.com/docker/docker/plugins"
 	"github.com/docker/docker/runconfig"
 	"github.com/docker/docker/utils"
 )
@@ -1330,7 +1332,51 @@ func (container *Container) waitForStart() error {
 		return err
 	}
 
+	if container.hostConfig.Plugin {
+		if err := container.waitForPluginSock(); err != nil {
+			return err
+		}
+	}
+
 	return nil
+}
+
+func (container *Container) waitForPluginSock() error {
+	pluginSock, err := container.getPluginSocketPath()
+	if err != nil {
+		return err
+	}
+
+	chConn := make(chan net.Conn)
+	chStop := make(chan struct{})
+	go func() {
+		log.Debugf("waiting for plugin socket at: %s", pluginSock)
+		for {
+			conn, err := net.DialTimeout("unix", pluginSock, 100*time.Millisecond)
+			// If the file doesn't exist yet, that's ok, maybe plugin hasn't created it yet
+			if err != nil {
+				select {
+				case <-chStop:
+					return
+				default:
+					continue
+				}
+			}
+			log.Debugf("got plugin socket")
+			chConn <- conn
+			return
+		}
+	}()
+
+	select {
+	case conn := <-chConn:
+		// We can close this net.Conn since the plugin system will establish it's own connection
+		conn.Close()
+		return plugins.Repo.RegisterPlugin(pluginSock)
+	case <-time.After(30 * time.Second):
+		chStop <- struct{}{}
+		return fmt.Errorf("connection to plugin sock timed out")
+	}
 }
 
 func (container *Container) allocatePort(eng *engine.Engine, port nat.Port, bindings nat.PortMap) error {
@@ -1414,4 +1460,8 @@ func (container *Container) getNetworkedContainer() (*Container, error) {
 
 func (container *Container) Stats() (*execdriver.ResourceStats, error) {
 	return container.daemon.Stats(container)
+}
+
+func (container *Container) getPluginSocketPath() (string, error) {
+	return container.getRootResourcePath(filepath.Join("p", "plugin.sock"))
 }

volumes/repository.go
@@ -1,6 +1,7 @@
 package volumes
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -9,8 +10,18 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/docker/docker/daemon/graphdriver"
+	"github.com/docker/docker/plugins"
 	"github.com/docker/docker/utils"
 )
+
+type VolumeExtensionReq struct {
+	HostPath    string
+	ContainerID string
+}
+
+type VolumeExtensionResp struct {
+	ModifiedHostPath string
+}
 
 type Repository struct {
 	configPath string
@@ -195,9 +206,39 @@ func (r *Repository) createNewVolumePath(id string) (string, error) {
 	return path, nil
 }
 
-func (r *Repository) FindOrCreateVolume(path string, writable bool) (*Volume, error) {
+func (r *Repository) FindOrCreateVolume(path, containerId string, writable bool) (*Volume, error) {
 	r.lock.Lock()
 	defer r.lock.Unlock()
+
+	plugins, err := plugins.Repo.GetPlugins("volume")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, plugin := range plugins {
+		data := VolumeExtensionReq{
+			HostPath:    path,
+			ContainerID: containerId,
+		}
+
+		resp, err := plugin.Call("POST", "volumes", data)
+		if err != nil {
+			return nil, fmt.Errorf("got error calling volume extension: %v", err)
+		}
+		defer resp.Close()
+
+		var extResp VolumeExtensionResp
+		log.Debugf("decoding volume extension response")
+		if err := json.NewDecoder(resp).Decode(&extResp); err != nil {
+			return nil, err
+		}
+
+		// Use the path provided by the extension instead of creating one
+		if extResp.ModifiedHostPath != "" {
+			log.Debugf("using modified host path for volume extension")
+			path = extResp.ModifiedHostPath
+		}
+	}
 
 	if path == "" {
 		return r.newVolume(path, writable)

tomwilkie/docker

daemon/container.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
 	"os"
 	"path"
 	"path/filepath"
@@ -40,6 +41,7 @@ import (
 	"github.com/docker/docker/pkg/stringid"
 	"github.com/docker/docker/pkg/symlink"
 	"github.com/docker/docker/pkg/ulimit"
+	"github.com/docker/docker/plugins"
 	"github.com/docker/docker/runconfig"
 	"github.com/docker/docker/utils"
 )
@@ -1454,7 +1456,51 @@ func (container *Container) waitForStart() error {
 		return err
 	}
 
+	if container.hostConfig.Plugin {
+		if err := container.waitForPluginSock(); err != nil {
+			return err
+		}
+	}
+
 	return nil
+}
+
+func (container *Container) waitForPluginSock() error {
+	pluginSock, err := container.getPluginSocketPath()
+	if err != nil {
+		return err
+	}
+
+	chConn := make(chan net.Conn)
+	chStop := make(chan struct{})
+	go func() {
+		logrus.Debugf("waiting for plugin socket at: %s", pluginSock)
+		for {
+			conn, err := net.DialTimeout("unix", pluginSock, 100*time.Millisecond)
+			// If the file doesn't exist yet, that's ok, maybe plugin hasn't created it yet
+			if err != nil {
+				select {
+				case <-chStop:
+					return
+				default:
+					continue
+				}
+			}
+			logrus.Debugf("got plugin socket")
+			chConn <- conn
+			return
+		}
+	}()
+
+	select {
+	case conn := <-chConn:
+		// We can close this net.Conn since the plugin system will establish it's own connection
+		conn.Close()
+		return plugins.Repo.RegisterPlugin(pluginSock)
+	case <-time.After(30 * time.Second):
+		chStop <- struct{}{}
+		return fmt.Errorf("connection to plugin sock timed out")
+	}
 }
 
 func (container *Container) allocatePort(eng *engine.Engine, port nat.Port, bindings nat.PortMap) error {
@@ -1533,4 +1579,8 @@ func (c *Container) LogDriverType() string {
 		return c.daemon.defaultLogConfig.Type
 	}
 	return c.hostConfig.LogConfig.Type
+}
+
+func (container *Container) getPluginSocketPath() (string, error) {
+	return container.getRootResourcePath(filepath.Join("p", "plugin.sock"))
 }

volumes/repository.go
@@ -1,6 +1,7 @@
 package volumes
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -10,7 +11,17 @@ import (
 	"github.com/Sirupsen/logrus"
 	"github.com/docker/docker/daemon/graphdriver"
 	"github.com/docker/docker/pkg/stringid"
+	"github.com/docker/docker/plugins"
 )
+
+type VolumeExtensionReq struct {
+	HostPath    string
+	ContainerID string
+}
+
+type VolumeExtensionResp struct {
+	ModifiedHostPath string
+}
 
 type Repository struct {
 	configPath string
@@ -179,9 +190,39 @@ func (r *Repository) createNewVolumePath(id string) (string, error) {
 	return path, nil
 }
 
-func (r *Repository) FindOrCreateVolume(path string, writable bool) (*Volume, error) {
+func (r *Repository) FindOrCreateVolume(path, containerId string, writable bool) (*Volume, error) {
 	r.lock.Lock()
 	defer r.lock.Unlock()
+
+	plugins, err := plugins.Repo.GetPlugins("volume")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, plugin := range plugins {
+		data := VolumeExtensionReq{
+			HostPath:    path,
+			ContainerID: containerId,
+		}
+
+		resp, err := plugin.Call("POST", "volumes", data)
+		if err != nil {
+			return nil, fmt.Errorf("got error calling volume extension: %v", err)
+		}
+		defer resp.Close()
+
+		var extResp VolumeExtensionResp
+		logrus.Debugf("decoding volume extension response")
+		if err := json.NewDecoder(resp).Decode(&extResp); err != nil {
+			return nil, err
+		}
+
+		// Use the path provided by the extension instead of creating one
+		if extResp.ModifiedHostPath != "" {
+			logrus.Debugf("using modified host path for volume extension")
+			path = extResp.ModifiedHostPath
+		}
+	}
 
 	if path == "" {
 		return r.newVolume(path, writable)