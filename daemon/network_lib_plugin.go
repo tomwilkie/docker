87d27904b732bdb8de46cdbad148d35d996a215e
Michael Bridgen <mikeb@squaremobius.net>
2015-04-22 11:15:19 +0100

    Use PUT instead of POST, and use serialisable structs
    libnetwork's driverapi.SandboxInfo contains structs (net.IPNet) that
    don't serialise to JSON straight-forwardly; so we expect strings from
    the plugin and parse them.
    
    Also: if we're asserting the existence of networks and endpoints by
    using their IDs in URLs, rather than asking for them to be created in
    a "directory", it's more appropriate to use PUT.

--- daemon/network_lib_plugin.go
+++ daemon/network_lib_plugin.go
@@ -3,23 +3,60 @@ package daemon
 import (
 	"encoding/json"
 	"fmt"
-
 	"github.com/Sirupsen/logrus"
 	"github.com/docker/docker/plugins"
 	"github.com/docker/libnetwork/driverapi"
+	"net"
 )
+
+type iface struct {
+	SrcName    string
+	DstName    string
+	Address    string
+	MACAddress string
+}
+
+type sbInfo struct {
+	Interfaces  []*iface
+	Gateway     net.IP
+	GatewayIPv6 net.IP
+}
+
+func (sb *sbInfo) toSandboxInfo() (*driverapi.SandboxInfo, error) {
+	var (
+		ifaces []*driverapi.Interface = make([]*driverapi.Interface, len(sb.Interfaces))
+	)
+	for i, inIf := range sb.Interfaces {
+		outIf := &driverapi.Interface{
+			SrcName:    inIf.SrcName,
+			DstName:    inIf.DstName,
+			MACAddress: inIf.MACAddress,
+		}
+		ip, ipnet, err := net.ParseCIDR(inIf.Address)
+		if err != nil {
+			return nil, err
+		}
+		ipnet.IP = ip
+		outIf.Address = ipnet
+		ifaces[i] = outIf
+	}
+	return &driverapi.SandboxInfo{
+		Interfaces:  ifaces,
+		Gateway:     nil,
+		GatewayIPv6: nil,
+	}, nil
+}
 
 type netLibDriver struct {
 	plugin *plugins.Plugin
 }
-
 
 func (driver *netLibDriver) Config(config interface{}) error {
 	return nil
 }
 
 func (driver *netLibDriver) CreateNetwork(nid driverapi.UUID, config interface{}) error {
-	reader, err := driver.plugin.Call("POST", string(nid), config)
+	reader, err := driver.plugin.Call("PUT", string(nid), config)
 	if err != nil {
 		logrus.Warningf("Driver returned err:", err)
 		return err
@@ -40,17 +77,25 @@ func (driver *netLibDriver) DeleteNetwork(nid driverapi.UUID) error {
 
 func (driver *netLibDriver) CreateEndpoint(nid, eid driverapi.UUID, key string, config interface{}) (*driverapi.SandboxInfo, error) {
 	path := fmt.Sprintf("%s/%s", nid, eid)
-	reader, err := driver.plugin.Call("POST", path, config)
+	reader, err := driver.plugin.Call("PUT", path, config)
 	if err != nil {
 		logrus.Warningf("Driver returned err:", err)
 		return nil, err
 	}
 	defer reader.Close()
-	var sbinfo driverapi.SandboxInfo
+	var sbinfo sbInfo
 	if err := json.NewDecoder(reader).Decode(&sbinfo); err != nil {
+		logrus.Warningf("Driver returned invalid JSON:", err)
 		return nil, err
 	}
-	return &sbinfo, nil
+
+	var sb *driverapi.SandboxInfo
+	if sb, err = sbinfo.toSandboxInfo(); err != nil {
+		logrus.Warningf("Unable to convert sbInfo")
+		return nil, err
+	}
+	logrus.Infof("Plugin returned %+v", sbinfo)
+	return sb, nil
 }
 
 func (driver *netLibDriver) DeleteEndpoint(nid, eid driverapi.UUID) error {
